00a96381c49da3cbec959f3515f5bfdc02a2cbeb
Patrick <patrickrieger019@gmail.com>
2020-01-16 13:37:54 -0300

    Aula 1 - Criada a base da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+
+mongoose.connect('mongodb+srv://patrick019:patrick019@cluster0-wlx7x.mongodb.net/week10?retryWrites=true&w=majority',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
