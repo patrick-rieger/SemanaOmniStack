9dc1cafa1c9cbeb454bac1d66f003f693e0c5835
Patrick <patrickrieger019@gmail.com>
2020-01-26 21:17:12 -0300

    Aula 4 - Funcionalidades avançadas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const http = require('http');
 const cors = require('cors');
+const {setupWebsocket} = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://patrick019:patrick019@cluster0-wlx7x.mongodb.net/week10?retryWrites=true&w=majority',
 {
@@ -14,4 +19,4 @@ mongoose.connect('mongodb+srv://patrick019:patrick019@cluster0-wlx7x.mongodb.net
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
+server.listen(3333);
